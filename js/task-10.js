4bdc7365f16b92ed6eda3c8fe771e1405c16a12c
DanielOsadchenko <dm.osadchenko@gmail.com>
2022-08-23 18:19:13 +0200

    completed task 10

--- js/task-10.js
+++ js/task-10.js
@@ -2,5 +2,30 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const createBtnRef = document.querySelector('[data-create]');
+const deleteBtnRef = document.querySelector('[data-destroy]');
+const amountBoxesRef = document.querySelector('#controls > input');
+const boxesContainerRef = document.querySelector('#boxes');
 
-// сделать 
+createBtnRef.addEventListener('click', createBoxes);
+deleteBtnRef.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  const boxes = [];
+  amount = amountBoxesRef.value;
+
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement('div');
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    boxes.push(box);
+  }
+
+  boxesContainerRef.append(...boxes);
+}
+
+function destroyBoxes() {
+  boxesContainerRef.innerHTML = "";
+  amountBoxesRef.value = 0;
+}
